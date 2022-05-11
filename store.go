2d836ecccb0d0035937e26d812516b62dadeb3f3
thorfour <thor.hansen@polarsignals.com>
2022-05-11 05:03:50 -0500

    save a file

--- store.go
+++ store.go
@@ -1,8 +1,12 @@
 package arcticdb
 
 import (
+	"bytes"
 	"context"
+	"io"
+	"io/ioutil"
 
+	"github.com/google/uuid"
 	"github.com/polarsignals/arcticdb/dynparquet"
 )
 
@@ -25,7 +29,41 @@ func WriteBlock(block *TableBlock) error {
 		return err
 	}
 
+	b := bytes.NewBuffer(nil)
+	cols := merged.DynamicColumns()
+	w, err := block.table.config.schema.NewWriter(b, cols)
+	if err != nil {
+		return err
+	}
+
 	// TODO do something with the merged row group
+	rows := merged.Rows()
+	n := 0
+	for {
+		row, err := rows.ReadRow(nil)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		err = w.WriteRow(row)
+		if err != nil {
+			return err
+		}
+		n++
+	}
+
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+
+	// Write the serialized buffer to disk
+	err = ioutil.WriteFile(uuid.New().String(), b.Bytes(), 0666)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
