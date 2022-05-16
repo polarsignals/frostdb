1212c05b4cc918f7596015b6c5848c776d2379bf
thorfour <thor.hansen@polarsignals.com>
2022-05-16 11:57:56 -0500

    unit tests for cancellation

--- table_test.go
+++ table_test.go
@@ -2,6 +2,7 @@ package arcticdb
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"math/rand"
 	"sync"
@@ -1012,4 +1013,162 @@ func Test_Table_Filter(t *testing.T) {
 	})
 	require.NoError(t, err)
 	require.False(t, iterated)
+}
+
+func Test_Table_InsertCancellation(t *testing.T) {
+	tests := map[string]struct {
+		granuleSize int
+	}{
+		"1024": {1024},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			table := basicTable(t, test.granuleSize)
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Millisecond)
+			defer cancel()
+
+			generateRows := func(n int) *dynparquet.Buffer {
+				rows := make(dynparquet.Samples, 0, n)
+				for i := 0; i < n; i++ {
+					rows = append(rows, dynparquet.Sample{
+						Labels: []dynparquet.Label{ // TODO would be nice to not have all the same column
+							{Name: "label1", Value: "value1"},
+							{Name: "label2", Value: "value2"},
+						},
+						Stacktrace: []uuid.UUID{
+							{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
+							{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2},
+						},
+						Timestamp: rand.Int63(),
+						Value:     rand.Int63(),
+					})
+				}
+				buf, err := rows.ToBuffer(table.Schema())
+				require.NoError(t, err)
+
+				buf.Sort()
+
+				// This is necessary because sorting a buffer makes concurrent reading not
+				// safe as the internal pages are cyclically sorted at read time. Cloning
+				// executes the cyclic sort once and makes the resulting buffer safe for
+				// concurrent reading as it no longer has to perform the cyclic sorting at
+				// read time. This should probably be improved in the parquet library.
+				buf, err = buf.Clone()
+				require.NoError(t, err)
+
+				return buf
+			}
+
+			// Spawn n workers that will insert values into the table
+			n := 4
+			inserts := 10
+			rows := 10
+			wg := &sync.WaitGroup{}
+			for i := 0; i < n; i++ {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					for i := 0; i < inserts; i++ {
+						if _, err := table.InsertBuffer(ctx, generateRows(rows)); err != nil {
+							if !errors.Is(err, context.DeadlineExceeded) {
+								fmt.Println("Received error on insert: ", err)
+							}
+						}
+					}
+				}()
+			}
+
+			wg.Wait()
+			table.Sync()
+
+			// Wait for the transaction pool to drain before iterating
+			pending := 0
+			table.db.txPool.Iterate(func(tx uint64) bool {
+				pending++
+				return false
+			})
+			for pending > 1 {
+				time.Sleep(30 * time.Millisecond) // sleep wait for pool to drain
+				pending = 0
+				table.db.txPool.Iterate(func(tx uint64) bool {
+					pending++
+					return false
+				})
+			}
+
+			totalrows := int64(0)
+			err := table.Iterator(context.Background(), memory.NewGoAllocator(), nil, nil, nil, func(ar arrow.Record) error {
+				totalrows += ar.NumRows()
+				defer ar.Release()
+
+				return nil
+			})
+			require.NoError(t, err)
+			require.Less(t, totalrows, int64(n*inserts*rows)) // We expect to cancel some subset of our writes
+		})
+	}
+}
+
+func Test_Table_CancelBasic(t *testing.T) {
+
+	table := basicTable(t, 8192)
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	samples := dynparquet.Samples{{
+		ExampleType: "test",
+		Labels: []dynparquet.Label{
+			{Name: "label1", Value: "value1"},
+			{Name: "label2", Value: "value2"},
+		},
+		Stacktrace: []uuid.UUID{
+			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
+			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2},
+		},
+		Timestamp: 1,
+		Value:     1,
+	}, {
+		ExampleType: "test",
+		Labels: []dynparquet.Label{
+			{Name: "label1", Value: "value2"},
+			{Name: "label2", Value: "value2"},
+			{Name: "label3", Value: "value3"},
+		},
+		Stacktrace: []uuid.UUID{
+			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
+			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2},
+		},
+		Timestamp: 2,
+		Value:     2,
+	}, {
+		ExampleType: "test",
+		Labels: []dynparquet.Label{
+			{Name: "label1", Value: "value3"},
+			{Name: "label2", Value: "value2"},
+			{Name: "label4", Value: "value4"},
+		},
+		Stacktrace: []uuid.UUID{
+			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
+			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2},
+		},
+		Timestamp: 3,
+		Value:     3,
+	}}
+
+	buf, err := samples.ToBuffer(table.Schema())
+	require.NoError(t, err)
+
+	_, err = table.InsertBuffer(ctx, buf)
+	require.True(t, errors.Is(err, context.Canceled))
+
+	totalrows := int64(0)
+	err = table.Iterator(context.Background(), memory.NewGoAllocator(), nil, nil, nil, func(ar arrow.Record) error {
+		totalrows += ar.NumRows()
+		defer ar.Release()
+
+		return nil
+	})
+	require.NoError(t, err)
+	require.Equal(t, int64(0), totalrows)
 }
