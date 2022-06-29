7d3c610ee20270278375651bbaf8ba1374a9a610
Frederic Branczyk <fbranczyk@gmail.com>
2022-06-29 19:17:51 +0200

    Expose prefixed bucket implementation

--- bucket.go
+++ bucket.go
@@ -9,50 +9,54 @@ import (
 	"github.com/thanos-io/objstore"
 )
 
-type BucketPrefixDecorator struct {
+type PrefixedBucket struct {
 	objstore.Bucket
 	prefix string
 }
 
-func (b *BucketPrefixDecorator) addPrefix(name string) string {
+func NewPrefixedBucket(bucket objstore.Bucket, prefix string) *PrefixedBucket {
+	return &PrefixedBucket{Bucket: bucket, prefix: prefix}
+}
+
+func (b *PrefixedBucket) addPrefix(name string) string {
 	return filepath.Join(b.prefix, name)
 }
 
-func (b *BucketPrefixDecorator) trimPrefix(name string) string {
+func (b *PrefixedBucket) trimPrefix(name string) string {
 	return strings.TrimPrefix(strings.TrimPrefix(name, b.prefix), string(filepath.Separator))
 }
 
-func (b *BucketPrefixDecorator) Iter(ctx context.Context, dir string, f func(string) error, options ...objstore.IterOption) error {
+func (b *PrefixedBucket) Iter(ctx context.Context, dir string, f func(string) error, options ...objstore.IterOption) error {
 	iterFunc := func(path string) error {
 		return f(b.trimPrefix(path))
 	}
 	return b.Bucket.Iter(ctx, b.addPrefix(dir), iterFunc, options...)
 }
 
-func (b *BucketPrefixDecorator) Get(ctx context.Context, name string) (io.ReadCloser, error) {
+func (b *PrefixedBucket) Get(ctx context.Context, name string) (io.ReadCloser, error) {
 	return b.Bucket.Get(ctx, b.addPrefix(name))
 }
 
-func (b *BucketPrefixDecorator) GetRange(ctx context.Context, name string, off, length int64) (io.ReadCloser, error) {
+func (b *PrefixedBucket) GetRange(ctx context.Context, name string, off, length int64) (io.ReadCloser, error) {
 	return b.Bucket.GetRange(ctx, b.addPrefix(name), off, length)
 }
 
-func (b *BucketPrefixDecorator) Exists(ctx context.Context, name string) (bool, error) {
+func (b *PrefixedBucket) Exists(ctx context.Context, name string) (bool, error) {
 	return b.Bucket.Exists(ctx, b.addPrefix(name))
 }
 
-func (b *BucketPrefixDecorator) Attributes(ctx context.Context, name string) (objstore.ObjectAttributes, error) {
+func (b *PrefixedBucket) Attributes(ctx context.Context, name string) (objstore.ObjectAttributes, error) {
 	return b.Bucket.Attributes(ctx, b.addPrefix(name))
 }
 
-func (b *BucketPrefixDecorator) Upload(ctx context.Context, name string, r io.Reader) error {
+func (b *PrefixedBucket) Upload(ctx context.Context, name string, r io.Reader) error {
 	return b.Bucket.Upload(ctx, b.addPrefix(name), r)
 }
 
-func (b *BucketPrefixDecorator) Delete(ctx context.Context, name string) error {
+func (b *PrefixedBucket) Delete(ctx context.Context, name string) error {
 	return b.Bucket.Delete(ctx, b.addPrefix(name))
 }
 
-func (b *BucketPrefixDecorator) Name() string {
+func (b *PrefixedBucket) Name() string {
 	return b.addPrefix(b.Name())
 }
