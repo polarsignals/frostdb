a59806c64edd91ae412024acfa9d48b4fef33ecf
thorfour <thor.hansen@polarsignals.com>
2022-07-01 08:37:37 -0500

    projection test

--- filter_test.go
+++ filter_test.go
@@ -2,6 +2,7 @@ package frostdb
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/apache/arrow/go/v8/arrow"
@@ -157,6 +158,106 @@ func TestFilter(t *testing.T) {
 				Execute(context.Background(), func(ar arrow.Record) error {
 					rows += ar.NumRows()
 					defer ar.Release()
+
+					return nil
+				})
+			require.NoError(t, err)
+			require.Equal(t, test.rows, rows)
+		})
+	}
+}
+
+func Test_Projection(t *testing.T) {
+	config := NewTableConfig(
+		dynparquet.NewSampleSchema(),
+	)
+
+	c := New(
+		nil,
+		8192,
+		512*1024*1024,
+	)
+	db, err := c.DB("test")
+	require.NoError(t, err)
+	table, err := db.Table("test", config, newTestLogger(t))
+	require.NoError(t, err)
+
+	samples := dynparquet.Samples{{
+		Labels: []dynparquet.Label{
+			{Name: "label1", Value: "value1"},
+			{Name: "label2", Value: "value2"},
+		},
+		Stacktrace: []uuid.UUID{
+			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
+			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2},
+		},
+		Timestamp: 1,
+		Value:     1,
+	}, {
+		Labels: []dynparquet.Label{
+			{Name: "label1", Value: "value2"},
+			{Name: "label2", Value: "value2"},
+			{Name: "label3", Value: "value3"},
+		},
+		Stacktrace: []uuid.UUID{
+			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
+			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2},
+		},
+		Timestamp: 2,
+		Value:     2,
+	}, {
+		Labels: []dynparquet.Label{
+			{Name: "label1", Value: "value3"},
+			{Name: "label2", Value: "value2"},
+			{Name: "label4", Value: "value4"},
+		},
+		Stacktrace: []uuid.UUID{
+			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
+			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2},
+		},
+		Timestamp: 3,
+		Value:     3,
+	}}
+
+	for i := 0; i < len(samples); i++ {
+		buf, err := samples[i : i+1].ToBuffer(table.Schema())
+		require.NoError(t, err)
+
+		_, err = table.InsertBuffer(context.Background(), buf)
+		require.NoError(t, err)
+	}
+
+	tests := map[string]struct {
+		filterExpr  logicalplan.Expr
+		projections []logicalplan.Expr
+		rows        int64
+	}{
+		">= int64": {
+			filterExpr: logicalplan.And(
+				logicalplan.Col("timestamp").GTE(logicalplan.Literal(2)),
+			),
+			projections: []logicalplan.Expr{logicalplan.DynCol("labels")},
+			rows:        6,
+		},
+	}
+
+	engine := query.NewEngine(
+		memory.NewGoAllocator(),
+		db.TableProvider(),
+	)
+
+	t.Parallel()
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			rows := int64(0)
+			err := engine.ScanTable("test").
+				Filter(test.filterExpr).
+				Project(test.projections...).
+				Execute(context.Background(), func(ar arrow.Record) error {
+					rows += ar.NumRows()
+					defer ar.Release()
+
+					fmt.Println(ar) // TODO REMOVE ME
 
 					return nil
 				})
