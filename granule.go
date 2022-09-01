9960850a25067fa929df7bd3be9f63cadd96f90c
thorfour <thor.hansen@polarsignals.com>
2022-08-31 20:48:25 -0500

    granule: implemented ColumnChunks

--- granule.go
+++ granule.go
@@ -335,32 +335,45 @@ func findMin(t parquet.Type, values []parquet.Value) *parquet.Value {
 // Schema implements the Particulate interface. It generates a parquet.Schema from the min/max fields of the Granule
 func (g *Granule) Schema() *parquet.Schema {
 	group := parquet.Group{}
-	func() {
-		g.metadata.maxlock.RLock()
-		defer g.metadata.maxlock.RUnlock()
-		for name, v := range g.metadata.max {
-			switch v.Kind() {
-			case parquet.Int32:
-				group[name] = parquet.Int(32)
-			case parquet.Int64:
-				group[name] = parquet.Int(64)
-			case parquet.Float:
-				group[name] = parquet.Leaf(parquet.FloatType)
-			case parquet.Double:
-				group[name] = parquet.Leaf(parquet.DoubleType)
-			case parquet.ByteArray:
-				group[name] = parquet.String()
-			case parquet.FixedLenByteArray:
-				group[name] = parquet.Leaf(parquet.ByteArrayType)
-			default:
-				group[name] = parquet.Leaf(parquet.DoubleType)
-			}
+	g.metadata.maxlock.RLock()
+	defer g.metadata.maxlock.RUnlock()
+	for name, v := range g.metadata.max {
+		switch v.Kind() {
+		case parquet.Int32:
+			group[name] = parquet.Int(32)
+		case parquet.Int64:
+			group[name] = parquet.Int(64)
+		case parquet.Float:
+			group[name] = parquet.Leaf(parquet.FloatType)
+		case parquet.Double:
+			group[name] = parquet.Leaf(parquet.DoubleType)
+		case parquet.ByteArray:
+			group[name] = parquet.String()
+		case parquet.FixedLenByteArray:
+			group[name] = parquet.Leaf(parquet.ByteArrayType)
+		default:
+			group[name] = parquet.Leaf(parquet.DoubleType)
 		}
-	}()
+	}
 	return parquet.NewSchema("granule", group)
 }
 
 // ColumnChunks implements the Particulate interface.
 func (g *Granule) ColumnChunks() []parquet.ColumnChunk {
-	return nil // TODO THOR
+	var chunks []parquet.ColumnChunk
+	g.metadata.maxlock.RLock()
+	defer g.metadata.maxlock.RUnlock()
+	g.metadata.minlock.RLock()
+	defer g.metadata.minlock.RUnlock()
+
+	for name, maxVal := range g.metadata.max {
+		chunks = append(chunks, VirtualSparseColumnChunk{
+			i: VirtualSparseColumnIndex{
+				Min: *g.metadata.min[name],
+				Max: *maxVal,
+			},
+		})
+	}
+
+	return chunks
 }
