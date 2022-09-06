8aac2628445610a9f93dff43d34088c72be149d9
Alfonso Subiotto Marques <alfonso.subiotto@polarsignals.com>
2022-09-06 11:52:54 +0200

    table: add missing rows.Close call
    The recent parquet version upgrade stressed a latent bug in our code. Lots of
    places were missing a rows.Close call, resulting in leaked goroutines.

--- table.go
+++ table.go
@@ -1027,6 +1027,7 @@ func (t *TableBlock) splitRowsByGranule(buf *dynparquet.SerializedBuffer) (map[*
 
 	// TODO: we might be able to do ascend less than or ascend greater than here?
 	rows := buf.DynamicRows()
+	defer rows.Close()
 	var prev *Granule
 	exhaustedAllRows := false
 
@@ -1216,6 +1217,7 @@ func (t *TableBlock) writeRows(w *dynparquet.PooledWriter, count int, rowGroups 
 	}
 
 	rows := merged.Rows()
+	defer rows.Close()
 	for {
 		rowsBuf := make([]parquet.Row, count)
 		n, err := rows.ReadRows(rowsBuf)
