81b9ab40dab572ce7c8479be3cd43e1e21238b7a
thorfour <thor.hansen@polarsignals.com>
2022-09-21 05:13:19 -0500

    handle definition level

--- dynparquet/arrow.go
+++ dynparquet/arrow.go
@@ -13,13 +13,18 @@ import (
 func ArrowRecordToBuffer(schema *Schema, r arrow.Record) (*Buffer, error) {
 	dynamicColumns := map[string][]string{}
 
-	for _, f := range r.Schema().Fields() {
-		names := strings.Split(f.Name, ".")
+	isDynamic := func(name string) (string, string, bool) {
+		names := strings.Split(name, ".")
 		if len(names) > 1 { // this may be a dynamic column
 			def, ok := schema.ColumnByName(names[0])
-			if ok && def.Dynamic {
-				dynamicColumns[names[0]] = append(dynamicColumns[names[0]], strings.Join(names[1:], "."))
-			}
+			return names[0], strings.Join(names[1:], "."), ok && def.Dynamic
+		}
+		return "", "", false
+	}
+
+	for _, f := range r.Schema().Fields() {
+		if col, val, ok := isDynamic(f.Name); ok {
+			dynamicColumns[col] = append(dynamicColumns[col], val)
 		}
 	}
 
@@ -34,22 +39,26 @@ func ArrowRecordToBuffer(schema *Schema, r arrow.Record) (*Buffer, error) {
 		row := []parquet.Value{}
 		for j := 0; int64(j) < r.NumCols(); j++ {
 			col := r.Column(j)
+			definitionLevel := 0
+			if _, _, ok := isDynamic(r.ColumnName(j)); ok {
+				definitionLevel = 1
+			}
 			switch col.DataType().ID() {
 			case arrow.BINARY:
 				a := col.(*array.Binary)
-				row = append(row, parquet.ValueOf(a.Value(i)).Level(0, 0, j))
+				row = append(row, parquet.ValueOf(a.Value(i)).Level(0, definitionLevel, j))
 			case arrow.STRING:
 				a := col.(*array.String)
-				row = append(row, parquet.ValueOf(a.Value(i)).Level(0, 0, j))
+				row = append(row, parquet.ValueOf(a.Value(i)).Level(0, definitionLevel, j))
 			case arrow.INT64:
 				a := col.(*array.Int64)
-				row = append(row, parquet.ValueOf(a.Value(i)).Level(0, 0, j))
+				row = append(row, parquet.ValueOf(a.Value(i)).Level(0, definitionLevel, j))
 			case arrow.UINT64:
 				a := col.(*array.Uint64)
-				row = append(row, parquet.ValueOf(a.Value(i)).Level(0, 0, j))
+				row = append(row, parquet.ValueOf(a.Value(i)).Level(0, definitionLevel, j))
 			case arrow.FLOAT64:
 				a := col.(*array.Float64)
-				row = append(row, parquet.ValueOf(a.Value(i)).Level(0, 0, j))
+				row = append(row, parquet.ValueOf(a.Value(i)).Level(0, definitionLevel, j))
 			default:
 				panic(fmt.Sprintf("at the disco %v", col.DataType().ID()))
 			}
