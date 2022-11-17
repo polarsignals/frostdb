a2221430e2d55ef48b9f56369cec577090f92f16
thorfour <thor.hansen@polarsignals.com>
2022-11-17 13:42:29 -0600

    fixes a panic caused by a nil active block

--- table.go
+++ table.go
@@ -242,6 +242,9 @@ func newTable(
 		Name: "index_size",
 		Help: "Number of granules in the table index currently.",
 	}, func() float64 {
+		if t.ActiveBlock() == nil {
+			return 0
+		}
 		return float64(t.ActiveBlock().Index().Len())
 	})
 
@@ -249,6 +252,9 @@ func newTable(
 		Name: "active_table_block_size",
 		Help: "Size of the active table block in bytes.",
 	}, func() float64 {
+		if t.ActiveBlock() == nil {
+			return 0
+		}
 		return float64(t.ActiveBlock().Size())
 	})
 
