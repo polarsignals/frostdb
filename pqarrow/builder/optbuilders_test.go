8c638861479f38e5361b0ebd29f74bd390d416c9
Alfonso Subiotto Marques <alfonso.subiotto@polarsignals.com>
2022-12-12 16:19:38 +0100

    pqarrow: fix RepeatLastValue
    This was broken in all optimized builders. Also added a test.

--- pqarrow/builder/optbuilders_test.go
+++ pqarrow/builder/optbuilders_test.go
@@ -23,3 +23,35 @@ func TestIssue270(t *testing.T) {
 	require.True(t, a.IsNull(0))
 	require.Equal(t, string(a.Value(1)), expString)
 }
+
+func TestRepeatLastValue(t *testing.T) {
+	testCases := []struct {
+		b builder.OptimizedBuilder
+		v any
+	}{
+		{
+			b: builder.NewOptBinaryBuilder(arrow.BinaryTypes.Binary),
+			v: []byte("hello"),
+		},
+		{
+			b: builder.NewOptInt64Builder(arrow.PrimitiveTypes.Int64),
+			v: int64(123),
+		},
+		{
+			b: builder.NewOptBooleanBuilder(arrow.FixedWidthTypes.Boolean),
+			v: true,
+		},
+	}
+	for _, tc := range testCases {
+		require.NoError(t, builder.AppendGoValue(tc.b, tc.v))
+		require.Equal(t, tc.b.Len(), 1)
+		tc.b.RepeatLastValue(9)
+		require.Equal(t, tc.b.Len(), 10)
+		a := tc.b.NewArray()
+		for i := 0; i < a.Len(); i++ {
+			v, err := builder.GetValue(a, i)
+			require.NoError(t, err)
+			require.Equal(t, tc.v, v)
+		}
+	}
+}
