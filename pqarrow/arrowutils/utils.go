d5de10986226d419dff184362a8d4b72517bbc67
Alfonso Subiotto Marques <alfonso.subiotto@polarsignals.com>
2022-12-29 13:32:50 +0100

    pqarrow: add ArrayConcatenator
    This is a simple helper struct to use in the place of a builder when the caller
    just wants to accumulate arrays and concatenate them at some point in the
    future. The need for this was discovered while building the OrderedAggregate.
    Calling NewArray on the builder and storing the result is not memory safe since
    following uses of the builder would overwrite the previous result.

--- pqarrow/arrowutils/utils.go
+++ pqarrow/arrowutils/utils.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/apache/arrow/go/v8/arrow"
 	"github.com/apache/arrow/go/v8/arrow/array"
+	"github.com/apache/arrow/go/v8/arrow/memory"
 )
 
 // GetValue returns the value at index i in arr. If the value is null, nil is
@@ -28,4 +29,28 @@ func GetValue(arr arrow.Array, i int) (any, error) {
 	default:
 		return nil, fmt.Errorf("unsupported type for GetValue %T", a)
 	}
+}
+
+// ArrayConcatenator is an object that helps callers keep track of a slice of
+// arrays and concatenate them into a single one when needed. This is more
+// efficient and memory safe than using a builder.
+type ArrayConcatenator struct {
+	arrs []arrow.Array
+}
+
+func (c *ArrayConcatenator) Add(arr arrow.Array) {
+	c.arrs = append(c.arrs, arr)
+}
+
+func (c *ArrayConcatenator) NewArray(mem memory.Allocator) (arrow.Array, error) {
+	arr, err := array.Concatenate(c.arrs, mem)
+	if err != nil {
+		return nil, err
+	}
+	c.arrs = c.arrs[:0]
+	return arr, err
+}
+
+func (c *ArrayConcatenator) Len() int {
+	return len(c.arrs)
 }
