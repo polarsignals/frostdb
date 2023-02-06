da874baaa8832d89977661d5cdf79d0ae19b5b03
thorfour <thor.hansen@polarsignals.com>
2023-02-06 10:21:30 -0600

    unit test

--- db_test.go
+++ db_test.go
@@ -81,169 +81,226 @@ func TestDBWithWALAndBucket(t *testing.T) {
 }
 
 func TestDBWithWAL(t *testing.T) {
-	config := NewTableConfig(
-		dynparquet.NewSampleSchema(),
-	)
-
-	logger := newTestLogger(t)
-
-	dir, err := os.MkdirTemp("", "frostdb-with-wal-test")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer os.RemoveAll(dir) // clean up
-
-	c, err := New(
-		WithLogger(logger),
-		WithWAL(),
-		WithStoragePath(dir),
-	)
-	require.NoError(t, err)
-	defer c.Close()
-
-	db, err := c.DB(context.Background(), "test")
-	require.NoError(t, err)
-	table, err := db.Table("test", config)
-	require.NoError(t, err)
-
-	samples := dynparquet.Samples{{
-		ExampleType: "test",
-		Labels: []dynparquet.Label{
-			{Name: "label1", Value: "value1"},
-			{Name: "label2", Value: "value2"},
-		},
-		Stacktrace: []uuid.UUID{
-			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
-			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2},
-		},
-		Timestamp: 1,
-		Value:     1,
-	}, {
-		ExampleType: "test",
-		Labels: []dynparquet.Label{
-			{Name: "label1", Value: "value2"},
-			{Name: "label2", Value: "value2"},
-			{Name: "label3", Value: "value3"},
-		},
-		Stacktrace: []uuid.UUID{
-			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
-			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2},
-		},
-		Timestamp: 2,
-		Value:     2,
-	}, {
-		ExampleType: "test",
-		Labels: []dynparquet.Label{
-			{Name: "label1", Value: "value3"},
-			{Name: "label2", Value: "value2"},
-			{Name: "label4", Value: "value4"},
-		},
-		Stacktrace: []uuid.UUID{
-			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
-			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2},
-		},
-		Timestamp: 3,
-		Value:     3,
-	}}
-
-	buf, err := samples.ToBuffer(table.Schema())
-	require.NoError(t, err)
-
 	ctx := context.Background()
-	_, err = table.InsertBuffer(ctx, buf)
-	require.NoError(t, err)
-
-	samples = dynparquet.Samples{{
-		ExampleType: "test",
-		Labels: []dynparquet.Label{
-			{Name: "label1", Value: "value1"},
-			{Name: "label2", Value: "value2"},
-		},
-		Stacktrace: []uuid.UUID{
-			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
-			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2},
-		},
-		Timestamp: 2,
-		Value:     2,
-	}}
-
-	buf, err = samples.ToBuffer(table.Schema())
-	require.NoError(t, err)
-
-	_, err = table.InsertBuffer(ctx, buf)
-	require.NoError(t, err)
-
-	samples = dynparquet.Samples{{
-		ExampleType: "test",
-		Labels: []dynparquet.Label{
-			{Name: "label1", Value: "value1"},
-			{Name: "label2", Value: "value2"},
-			{Name: "label3", Value: "value3"},
-		},
-		Stacktrace: []uuid.UUID{
-			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
-			{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2},
-		},
-		Timestamp: 3,
-		Value:     3,
-	}}
-
-	buf, err = samples.ToBuffer(table.Schema())
-	require.NoError(t, err)
-
-	_, err = table.InsertBuffer(ctx, buf)
-	require.NoError(t, err)
-
-	require.NoError(t, c.Close())
-
-	c, err = New(
-		WithLogger(logger),
-		WithWAL(),
-		WithStoragePath(dir),
-	)
-	require.NoError(t, err)
-	defer c.Close()
-
-	require.NoError(t, c.ReplayWALs(context.Background()))
-
-	db, err = c.DB(context.Background(), "test")
-	require.NoError(t, err)
-	table, err = db.Table("test", config)
-	require.NoError(t, err)
-
-	pool := memory.NewGoAllocator()
-	err = table.View(ctx, func(ctx context.Context, tx uint64) error {
-		return table.Iterator(
-			ctx,
-			tx,
-			pool,
-			logicalplan.IterOptions{},
-			[]logicalplan.Callback{func(ctx context.Context, ar arrow.Record) error {
-				t.Log(ar)
-				defer ar.Release()
-				return nil
-			}},
+	test := func(t *testing.T, isArrow bool) {
+		config := NewTableConfig(
+			dynparquet.NewSampleSchema(),
 		)
+
+		logger := newTestLogger(t)
+
+		dir, err := os.MkdirTemp("", "frostdb-with-wal-test")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer os.RemoveAll(dir) // clean up
+
+		c, err := New(
+			WithLogger(logger),
+			WithWAL(),
+			WithStoragePath(dir),
+		)
+		require.NoError(t, err)
+		defer c.Close()
+
+		db, err := c.DB(context.Background(), "test")
+		require.NoError(t, err)
+		table, err := db.Table("test", config)
+		require.NoError(t, err)
+
+		samples := dynparquet.Samples{{
+			ExampleType: "test",
+			Labels: []dynparquet.Label{
+				{Name: "label1", Value: "value1"},
+				{Name: "label2", Value: "value2"},
+			},
+			Stacktrace: []uuid.UUID{
+				{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
+				{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2},
+			},
+			Timestamp: 1,
+			Value:     1,
+		}, {
+			ExampleType: "test",
+			Labels: []dynparquet.Label{
+				{Name: "label1", Value: "value2"},
+				{Name: "label2", Value: "value2"},
+				{Name: "label3", Value: "value3"},
+			},
+			Stacktrace: []uuid.UUID{
+				{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
+				{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2},
+			},
+			Timestamp: 2,
+			Value:     2,
+		}, {
+			ExampleType: "test",
+			Labels: []dynparquet.Label{
+				{Name: "label1", Value: "value3"},
+				{Name: "label2", Value: "value2"},
+				{Name: "label4", Value: "value4"},
+			},
+			Stacktrace: []uuid.UUID{
+				{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
+				{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2},
+			},
+			Timestamp: 3,
+			Value:     3,
+		}}
+
+		switch isArrow {
+		case true:
+
+			ps, err := table.Schema().DynamicParquetSchema(map[string][]string{
+				"labels": {"label1", "label2", "label3", "label4"},
+			})
+			require.NoError(t, err)
+
+			sc, err := pqarrow.ParquetSchemaToArrowSchema(ctx, ps, logicalplan.IterOptions{})
+			require.NoError(t, err)
+
+			rec, err := samples.ToRecord(sc)
+			require.NoError(t, err)
+
+			ctx := context.Background()
+			_, err = table.InsertRecord(ctx, rec)
+			require.NoError(t, err)
+
+		default:
+			buf, err := samples.ToBuffer(table.Schema())
+			require.NoError(t, err)
+
+			ctx := context.Background()
+			_, err = table.InsertBuffer(ctx, buf)
+			require.NoError(t, err)
+		}
+
+		samples = dynparquet.Samples{{
+			ExampleType: "test",
+			Labels: []dynparquet.Label{
+				{Name: "label1", Value: "value1"},
+				{Name: "label2", Value: "value2"},
+			},
+			Stacktrace: []uuid.UUID{
+				{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
+				{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2},
+			},
+			Timestamp: 2,
+			Value:     2,
+		}}
+
+		switch isArrow {
+		case true:
+			ps, err := table.Schema().DynamicParquetSchema(map[string][]string{
+				"labels": {"label1", "label2"},
+			})
+			require.NoError(t, err)
+
+			sc, err := pqarrow.ParquetSchemaToArrowSchema(ctx, ps, logicalplan.IterOptions{})
+			require.NoError(t, err)
+
+			rec, err := samples.ToRecord(sc)
+			require.NoError(t, err)
+
+			ctx := context.Background()
+			_, err = table.InsertRecord(ctx, rec)
+			require.NoError(t, err)
+		default:
+			buf, err := samples.ToBuffer(table.Schema())
+			require.NoError(t, err)
+
+			_, err = table.InsertBuffer(ctx, buf)
+			require.NoError(t, err)
+		}
+
+		samples = dynparquet.Samples{{
+			ExampleType: "test",
+			Labels: []dynparquet.Label{
+				{Name: "label1", Value: "value1"},
+				{Name: "label2", Value: "value2"},
+				{Name: "label3", Value: "value3"},
+			},
+			Stacktrace: []uuid.UUID{
+				{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
+				{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2},
+			},
+			Timestamp: 3,
+			Value:     3,
+		}}
+
+		switch isArrow {
+		case true:
+			ps, err := table.Schema().DynamicParquetSchema(map[string][]string{
+				"labels": {"label1", "label2", "label3"},
+			})
+			require.NoError(t, err)
+
+			sc, err := pqarrow.ParquetSchemaToArrowSchema(ctx, ps, logicalplan.IterOptions{})
+			require.NoError(t, err)
+
+			rec, err := samples.ToRecord(sc)
+			require.NoError(t, err)
+
+			ctx := context.Background()
+			_, err = table.InsertRecord(ctx, rec)
+			require.NoError(t, err)
+		default:
+			buf, err := samples.ToBuffer(table.Schema())
+			require.NoError(t, err)
+
+			_, err = table.InsertBuffer(ctx, buf)
+			require.NoError(t, err)
+		}
+
+		require.NoError(t, c.Close())
+
+		c, err = New(
+			WithLogger(logger),
+			WithWAL(),
+			WithStoragePath(dir),
+		)
+		require.NoError(t, err)
+		defer c.Close()
+
+		require.NoError(t, c.ReplayWALs(context.Background()))
+
+		db, err = c.DB(context.Background(), "test")
+		require.NoError(t, err)
+		table, err = db.Table("test", config)
+		require.NoError(t, err)
+
+		pool := memory.NewGoAllocator()
+		records := []arrow.Record{}
+		err = table.View(ctx, func(ctx context.Context, tx uint64) error {
+			return table.Iterator(
+				ctx,
+				tx,
+				pool,
+				logicalplan.IterOptions{},
+				[]logicalplan.Callback{func(ctx context.Context, ar arrow.Record) error {
+					ar.Retain()
+					records = append(records, ar)
+					return nil
+				}},
+			)
+		})
+		require.NoError(t, err)
+
+		// Validate returned data
+		rows := int64(0)
+		for _, r := range records {
+			rows += r.NumRows()
+			r.Release()
+		}
+		require.Equal(t, int64(5), rows)
+	}
+
+	t.Run("parquet", func(t *testing.T) {
+		test(t, false)
 	})
-	require.NoError(t, err)
-
-	uuid1 := uuid.MustParse("00000000-0000-0000-0000-000000000001")
-	uuid2 := uuid.MustParse("00000000-0000-0000-0000-000000000002")
-
-	// One granule with 3 parts
-	require.Equal(t, 1, table.active.Index().Len())
-	require.Equal(t, 3, table.active.Index().Min().(*Granule).parts.Total())
-	require.Equal(t, parquet.Row{
-		parquet.ValueOf("test").Level(0, 0, 0),
-		parquet.ValueOf("value1").Level(0, 1, 1),
-		parquet.ValueOf("value2").Level(0, 1, 2),
-		parquet.ValueOf(nil).Level(0, 0, 3),
-		parquet.ValueOf(nil).Level(0, 0, 4),
-		parquet.ValueOf(append(uuid1[:], uuid2[:]...)).Level(0, 0, 5),
-		parquet.ValueOf(1).Level(0, 0, 6),
-		parquet.ValueOf(1).Level(0, 0, 7),
-	}, (*dynparquet.DynamicRow)(table.active.Index().Min().(*Granule).metadata.least).Row)
-	require.Equal(t, 1, table.active.Index().Len())
+	t.Run("arrow", func(t *testing.T) {
+		test(t, true)
+	})
 }
 
 func Test_DB_WithStorage(t *testing.T) {
