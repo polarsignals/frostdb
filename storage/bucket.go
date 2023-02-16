120a456910830436da6f4f3f99c29e5e37151b57
Matthias Loibl <mail@matthiasloibl.com>
2023-02-16 13:17:05 +0100

    Update storage/bucket.go
    Co-authored-by: Alfonso Subiotto Marqués <alfonso.subiotto@polarsignals.com>

--- storage/bucket.go
+++ storage/bucket.go
@@ -59,8 +59,11 @@ func (b *FileReaderAt) ReadAt(p []byte, off int64) (n int, err error) {
 	for total < len(p) { // Read does not guarantee the buffer will be full, but ReadAt does
 		n, err = rc.Read(p[total:])
 		total += n
-		// If io.EOF is returned it means we read the end of the file and simply return the total
-		if err != nil && err != io.EOF {
+		if err != nil {
+		    if errors.Is(err, io.EOF) {
+		        // If io.EOF is returned it means we read the end of the file and simply return the total.
+		        break
+		    }
 			return total, err
 		}
 	}
