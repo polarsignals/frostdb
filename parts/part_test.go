da93c43494134a396e0782cc00a28b453baa8476
Alfonso Subiotto Marques <alfonso.subiotto@polarsignals.com>
2023-09-19 13:36:27 +0200

    parts: add FindMaximumNonOverlappingSet
    This gives callers an O(n*logn) way to find the minimum overlapping set of
    parts.

---
+++ parts/part_test.go
@@ -0,0 +1,95 @@
+package parts
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/polarsignals/frostdb/dynparquet"
+	schemapb "github.com/polarsignals/frostdb/gen/proto/go/frostdb/schema/v1alpha1"
+)
+
+func TestFindMaximumNonOverlappingSet(t *testing.T) {
+	testSchema, err := dynparquet.SchemaFromDefinition(&schemapb.Schema{
+		Name: "test_schema",
+		Columns: []*schemapb.Column{{
+			Name: "ints",
+			StorageLayout: &schemapb.StorageLayout{
+				Type:     schemapb.StorageLayout_TYPE_INT64,
+				Encoding: schemapb.StorageLayout_ENCODING_PLAIN_UNSPECIFIED,
+			},
+		}},
+		SortingColumns: []*schemapb.SortingColumn{{Name: "ints", Direction: schemapb.SortingColumn_DIRECTION_ASCENDING}},
+	})
+	require.NoError(t, err)
+
+	type rng struct {
+		start int64
+		end   int64
+	}
+	type dataModel struct {
+		Ints int64
+	}
+	for _, tc := range []struct {
+		name                   string
+		ranges                 []rng
+		expectedNonOverlapping []rng
+		expectedOverlapping    []rng
+	}{
+		{
+			name:                   "SinglePart",
+			ranges:                 []rng{{1, 2}},
+			expectedNonOverlapping: []rng{{1, 2}},
+		},
+		{
+			name:                   "TwoNonOverlapping",
+			ranges:                 []rng{{1, 2}, {3, 4}},
+			expectedNonOverlapping: []rng{{1, 2}, {3, 4}},
+		},
+		{
+			name:                   "OneOverlap",
+			ranges:                 []rng{{1, 2}, {4, 7}, {3, 8}},
+			expectedNonOverlapping: []rng{{1, 2}, {4, 7}},
+			expectedOverlapping:    []rng{{3, 8}},
+		},
+		{
+			name:                   "ChooseMinimumNumber",
+			ranges:                 []rng{{1, 2}, {4, 10}, {4, 5}, {6, 7}},
+			expectedNonOverlapping: []rng{{1, 2}, {4, 5}, {6, 7}},
+			expectedOverlapping:    []rng{{4, 10}},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			parts := make([]*Part, len(tc.ranges))
+			for i := range parts {
+				start := dataModel{Ints: tc.ranges[i].start}
+				end := dataModel{Ints: tc.ranges[i].end}
+				buf, err := dynparquet.ValuesToBuffer(testSchema, start, end)
+				require.NoError(t, err)
+				var b bytes.Buffer
+				require.NoError(t, testSchema.SerializeBuffer(&b, buf))
+				serBuf, err := dynparquet.ReaderFromBytes(b.Bytes())
+				require.NoError(t, err)
+				parts[i] = NewPart(0, serBuf)
+			}
+			nonOverlapping, overlapping, err := FindMaximumNonOverlappingSet(testSchema, parts)
+			require.NoError(t, err)
+
+			verify := func(t *testing.T, expected []rng, actual []*Part) {
+				t.Helper()
+				require.Len(t, actual, len(expected))
+				for i := range actual {
+					start, err := actual[i].Least()
+					require.NoError(t, err)
+					end, err := actual[i].most()
+					require.NoError(t, err)
+					require.Equal(t, expected[i].start, start.Row[0].Int64())
+					require.Equal(t, expected[i].end, end.Row[0].Int64())
+				}
+			}
+			verify(t, tc.expectedNonOverlapping, nonOverlapping)
+			verify(t, tc.expectedOverlapping, overlapping)
+		})
+	}
+}
