672bc29f4b8ce48cc2585f1f82537ee2c597ea76
Matthias Loibl <mail@matthiasloibl.com>
2023-12-14 12:05:43 +0100

    pqarrow/arrowutils: Add SortRecord and ReorderRecord
    This is extract from a previous PR #461.

---
+++ pqarrow/arrowutils/sort.go
@@ -0,0 +1,105 @@
+package arrowutils
+
+import (
+	"bytes"
+	"fmt"
+	"sort"
+
+	"github.com/apache/arrow/go/v14/arrow"
+	"github.com/apache/arrow/go/v14/arrow/array"
+	"github.com/apache/arrow/go/v14/arrow/memory"
+	"golang.org/x/exp/constraints"
+
+	"github.com/polarsignals/frostdb/pqarrow/builder"
+)
+
+// SortRecord sorts the given record's rows by the given column. Currently only supports int64, string and binary columns.
+func SortRecord(r arrow.Record, col int) ([]int, error) {
+	if r.NumRows() == 0 {
+		return nil, nil
+	}
+	if r.NumRows() == 1 {
+		return []int{0}, nil
+	}
+
+	indices := make([]int, r.NumRows())
+	// populate indices
+	for i := range indices {
+		indices[i] = i
+	}
+
+	switch c := r.Column(col).(type) {
+	case *array.Int64:
+		sort.Sort(orderedSorter[int64]{array: c, indices: indices})
+	case *array.String:
+		sort.Sort(orderedSorter[string]{array: c, indices: indices})
+	case *array.Binary:
+		sort.Sort(binarySort{array: c, indices: indices})
+	default:
+		return nil, fmt.Errorf("unsupported column type for sorting %T", c)
+	}
+
+	return indices, nil
+}
+
+// ReorderRecord reorders the given record's rows by the given indices.
+func ReorderRecord(mem memory.Allocator, r arrow.Record, indices []int) (arrow.Record, error) {
+	// if the indices are already sorted, we can return the original record to save memory allocations
+	if sort.SliceIsSorted(indices, func(i, j int) bool { return indices[i] < indices[j] }) {
+		return r, nil
+	}
+
+	recordBuilder := builder.NewRecordBuilder(mem, r.Schema())
+	recordBuilder.Reserve(int(r.NumRows()))
+	for i := 0; i < int(r.NumRows()); i++ {
+		for colIdx, b := range recordBuilder.Fields() {
+			// here we read the value from the original record,
+			// but we the correct index and then write it to the new record
+			if err := builder.AppendValue(b, r.Column(colIdx), indices[i]); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return recordBuilder.NewRecord(), nil
+}
+
+type orderedArray[T constraints.Ordered] interface {
+	Value(int) T
+	Len() int
+}
+
+type orderedSorter[T constraints.Ordered] struct {
+	array   orderedArray[T]
+	indices []int
+}
+
+func (s orderedSorter[T]) Len() int {
+	return s.array.Len()
+}
+
+func (s orderedSorter[T]) Less(i, j int) bool {
+	return s.array.Value(s.indices[i]) < s.array.Value(s.indices[j])
+}
+
+func (s orderedSorter[T]) Swap(i, j int) {
+	s.indices[i], s.indices[j] = s.indices[j], s.indices[i]
+}
+
+type binarySort struct {
+	array   *array.Binary
+	indices []int
+}
+
+func (s binarySort) Len() int {
+	return s.array.Len()
+}
+
+func (s binarySort) Less(i, j int) bool {
+	// we need to read the indices from the indices slice, as they might have already been swapped.
+	return bytes.Compare(s.array.Value(s.indices[i]), s.array.Value(s.indices[j])) == -1
+}
+
+func (s binarySort) Swap(i, j int) {
+	s.indices[i], s.indices[j] = s.indices[j], s.indices[i]
+}
---
+++ pqarrow/arrowutils/sort_test.go
@@ -0,0 +1,77 @@
+package arrowutils
+
+import (
+	"testing"
+
+	"github.com/apache/arrow/go/v14/arrow"
+	"github.com/apache/arrow/go/v14/arrow/array"
+	"github.com/apache/arrow/go/v14/arrow/memory"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSortRecord(t *testing.T) {
+	schema := arrow.NewSchema(
+		[]arrow.Field{
+			{Name: "int", Type: arrow.PrimitiveTypes.Int64},
+			{Name: "string", Type: arrow.BinaryTypes.String},
+		},
+		nil,
+	)
+
+	mem := memory.DefaultAllocator
+	ib := array.NewInt64Builder(mem)
+	ib.Append(0)
+	ib.Append(3)
+	ib.Append(5)
+	ib.Append(1)
+
+	sb := array.NewStringBuilder(mem)
+	sb.Append("d")
+	sb.Append("c")
+	sb.Append("b")
+	sb.Append("a")
+
+	record := array.NewRecord(schema, []arrow.Array{ib.NewArray(), sb.NewArray()}, int64(4))
+
+	// Sort the record by the first column - int64
+	{
+		sortedIndices, err := SortRecord(record, 0)
+		require.NoError(t, err)
+		require.Equal(t, []int{0, 3, 1, 2}, sortedIndices)
+
+		sortedByInts, err := ReorderRecord(mem, record, sortedIndices)
+		require.NoError(t, err)
+
+		// check that the column got sortedIndices
+		intCol := sortedByInts.Column(0).(*array.Int64)
+		require.Equal(t, []int64{0, 1, 3, 5}, intCol.Int64Values())
+		// make sure the other column got updated too
+		strings := make([]string, 4)
+		stringCol := sortedByInts.Column(1).(*array.String)
+		for i := 0; i < 4; i++ {
+			strings[i] = stringCol.Value(i)
+		}
+		require.Equal(t, []string{"d", "a", "c", "b"}, strings)
+	}
+
+	// Sort the record by the second column - string
+	{
+		sortedIndices, err := SortRecord(record, 1)
+		require.NoError(t, err)
+		require.Equal(t, []int{3, 2, 1, 0}, sortedIndices)
+
+		sortedByStrings, err := ReorderRecord(mem, record, sortedIndices)
+		require.NoError(t, err)
+
+		// check that the column got sortedByInts
+		intCol := sortedByStrings.Column(0).(*array.Int64)
+		require.Equal(t, []int64{1, 5, 3, 0}, intCol.Int64Values())
+		// make sure the other column got updated too
+		strings := make([]string, 4)
+		stringCol := sortedByStrings.Column(1).(*array.String)
+		for i := 0; i < 4; i++ {
+			strings[i] = stringCol.Value(i)
+		}
+		require.Equal(t, []string{"a", "b", "c", "d"}, strings)
+	}
+}
