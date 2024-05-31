7409fa1a9ed50439e5513ea7c32332c9b1f33c51
thorfour <me@thor-hansen.com>
2024-05-31 10:58:30 -0500

    sampler randomness check

--- query/physicalplan/sampler_test.go
+++ query/physicalplan/sampler_test.go
@@ -86,3 +86,54 @@ func Test_Sampler(t *testing.T) {
 		})
 	}
 }
+
+// Test_Sampler_Randomness tests the randomness of the sampler by checking the distribution of the samples.
+func Test_Sampler_Randomness(t *testing.T) {
+	ctx := context.Background()
+	reservoirSize := int64(10)
+	numRows := int64(100)
+	recordSize := int64(1)
+	iterations := int64(10_000)
+	bins := make([]int64, numRows)
+
+	// Create a new sampler
+	for i := int64(0); i < iterations; i++ {
+		s := NewReservoirSampler(reservoirSize)
+		s.SetNext(&TestPlan{
+			callback: func(ctx context.Context, r arrow.Record) error {
+				for _, v := range r.Column(0).(*array.Int64).Int64Values() {
+					bins[v]++
+				}
+				return nil
+			},
+		})
+
+		schema := arrow.NewSchema([]arrow.Field{
+			{Name: "a", Type: arrow.PrimitiveTypes.Int64},
+		}, nil)
+		bldr := array.NewRecordBuilder(memory.NewGoAllocator(), schema)
+
+		for i := int64(0); i < numRows/recordSize; i++ {
+			for j := int64(0); j < recordSize; j++ {
+				bldr.Field(0).(*array.Int64Builder).Append(int64((i * recordSize) + j))
+			}
+			r := bldr.NewRecord()
+			t.Cleanup(r.Release)
+			require.NoError(t, s.Callback(ctx, r))
+		}
+
+		require.NoError(t, s.Finish(ctx))
+	}
+
+	// Any given number has a reservoirSize/numRows or for current settings a 10/100 chance of being selected
+	// 10/100 * 10000 iterations = 1000. So we expect each number to be selected roughly 1000 times.
+	// Using a tolerance of 10% we expect each number to be selected between 900 and 1100 times.
+	tolerance := 0.10
+	expectation := float64(reservoirSize) / float64(numRows) * float64(iterations)
+	lowerBound := expectation - expectation*tolerance
+	upperBound := expectation + expectation*tolerance
+	for _, count := range bins {
+		require.GreaterOrEqual(t, float64(count), lowerBound)
+		require.LessOrEqual(t, float64(count), upperBound)
+	}
+}
