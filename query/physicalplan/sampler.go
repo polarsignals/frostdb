44887acae7710a16a08ffd9e99aa4f85eaa5d692
thorfour <me@thor-hansen.com>
2024-06-20 11:16:18 -0500

    refactor: samples to use a pointer to a struct

--- query/physicalplan/sampler.go
+++ query/physicalplan/sampler.go
@@ -36,29 +36,36 @@ type ReservoirSampler struct {
 }
 
 type sample struct {
-	// Record is a pointer to the record that is being sampled
-	arrow.Record
 	// i is the index of the row in the record that is being sampled. If i is -1, the entire record is being sampled.
 	i int64
-	// ref is the number of references to the record. When ref is 0, the record can be released.
-	ref *int64
+	// ref to the record that is being sampled
+	ref *referencedRecord
 }
 
-func (s sample) Release() int64 {
+type referencedRecord struct {
+	// Record is a pointer to the record that is being sampled
+	arrow.Record
+	// size is the size of the record in bytes
+	size int64
+	// ref is the number of references to the record. When ref is 0, the record can be released.
+	ref int64
+}
+
+func (s *referencedRecord) Release() int64 {
 	defer s.Record.Release()
-	*s.ref--
-	if *s.ref == 0 {
-		return util.TotalRecordSize(s.Record) // Record is being released from the reservoir. Return the size of the record for accounting purposes.
+	s.ref--
+	if s.ref == 0 {
+		return s.size
 	}
 
 	return 0
 }
 
-func (s sample) Retain() int64 {
+func (s *referencedRecord) Retain() int64 {
 	defer s.Record.Retain()
-	*s.ref++
-	if *s.ref == 1 {
-		return util.TotalRecordSize(s.Record) // Record is being retained in the reservoir. Return the size of the record for accounting purposes.
+	s.ref++
+	if s.ref == 1 {
+		return s.size
 	}
 	return 0
 }
@@ -88,14 +95,14 @@ func (s *ReservoirSampler) Draw() *Diagram {
 
 func (s *ReservoirSampler) Close() {
 	for _, r := range s.reservoir {
-		s.sizeInBytes -= r.Release()
+		s.sizeInBytes -= r.ref.Release()
 	}
 	s.next.Close()
 }
 
 // Callback collects all the records to sample.
 func (s *ReservoirSampler) Callback(_ context.Context, r arrow.Record) error {
-	var ref *int64
+	var ref *referencedRecord
 	r, ref = s.fill(r)
 	if r == nil { // The record fit in the reservoir
 		return nil
@@ -114,30 +121,35 @@ func (s *ReservoirSampler) Callback(_ context.Context, r arrow.Record) error {
 	return nil
 }
 
-func refPtr() *int64 {
-	ref := new(int64)
-	return ref
+func refPtr(r arrow.Record) *referencedRecord {
+	return &referencedRecord{Record: r, size: util.TotalRecordSize(r)}
 }
 
 // fill will fill the reservoir with the first size records.
-func (s *ReservoirSampler) fill(r arrow.Record) (arrow.Record, *int64) {
+func (s *ReservoirSampler) fill(r arrow.Record) (arrow.Record, *referencedRecord) {
 	if s.n >= s.size {
-		return r, refPtr()
+		return r, refPtr(r)
 	}
 
 	if s.n+r.NumRows() <= s.size { // The record fits in the reservoir
-		smpl := sample{Record: r, i: -1, ref: refPtr()}
+		smpl := sample{
+			i:   -1,
+			ref: refPtr(r),
+		}
 		s.reservoir = append(s.reservoir, smpl)
-		s.sizeInBytes += smpl.Retain()
+		s.sizeInBytes += smpl.ref.Retain()
 		s.n += r.NumRows()
 		return nil, nil
 	}
 
 	// The record partially fits in the reservoir
-	ref := refPtr()
-	smpl := sample{Record: r.NewSlice(0, s.size-s.n), i: -1, ref: ref}
+	ref := refPtr(r)
+	smpl := sample{
+		i:   -1,
+		ref: refPtr(r.NewSlice(0, s.size-s.n)),
+	}
 	s.reservoir = append(s.reservoir, smpl)
-	s.sizeInBytes += smpl.Retain()
+	s.sizeInBytes += smpl.ref.Retain()
 	r = r.NewSlice(s.size-s.n, r.NumRows())
 	s.n = s.size
 	return r, ref
@@ -145,25 +157,28 @@ func (s *ReservoirSampler) fill(r arrow.Record) (arrow.Record, *int64) {
 
 func (s *ReservoirSampler) sliceReservoir() {
 	newReservoir := make([]sample, 0, s.size)
-	ref := refPtr()
 	for _, r := range s.reservoir {
-		for j := int64(0); j < r.Record.NumRows(); j++ {
-			smpl := sample{Record: r.Record, i: j, ref: ref}
+		ref := refPtr(r.ref.Record)
+		for j := int64(0); j < r.ref.Record.NumRows(); j++ {
+			smpl := sample{
+				i:   j,
+				ref: ref,
+			}
 			newReservoir = append(newReservoir, smpl)
-			s.sizeInBytes += smpl.Retain()
+			s.sizeInBytes += smpl.ref.Retain()
 		}
-		s.sizeInBytes -= r.Release()
+		s.sizeInBytes -= r.ref.Release()
 	}
 	s.reservoir = newReservoir
 }
 
 // sample implements the reservoir sampling algorithm found https://en.wikipedia.org/wiki/Reservoir_sampling.
-func (s *ReservoirSampler) sample(r arrow.Record, ref *int64) {
+func (s *ReservoirSampler) sample(r arrow.Record, ref *referencedRecord) {
 	n := s.n + r.NumRows()
 	if s.i == 0 {
 		s.i = float64(s.n) - 1
 	} else if s.i < float64(n) {
-		s.replace(rand.Intn(int(s.size)), sample{Record: r, i: int64(s.i) - s.n, ref: ref})
+		s.replace(rand.Intn(int(s.size)), sample{i: int64(s.i) - s.n, ref: ref})
 		s.w = s.w * math.Exp(math.Log(rand.Float64())/float64(s.size))
 	}
 
@@ -171,7 +186,7 @@ func (s *ReservoirSampler) sample(r arrow.Record, ref *int64) {
 		s.i += math.Floor(math.Log(rand.Float64())/math.Log(1-s.w)) + 1
 		if s.i < float64(n) {
 			// replace a random item of the reservoir with row i
-			s.replace(rand.Intn(int(s.size)), sample{Record: r, i: int64(s.i) - s.n, ref: ref})
+			s.replace(rand.Intn(int(s.size)), sample{i: int64(s.i) - s.n, ref: ref})
 			s.w = s.w * math.Exp(math.Log(rand.Float64())/float64(s.size))
 		}
 	}
@@ -183,13 +198,13 @@ func (s *ReservoirSampler) Finish(ctx context.Context) error {
 	// Send all the records in the reservoir to the next operator
 	for _, r := range s.reservoir {
 		if r.i == -1 {
-			if err := s.next.Callback(ctx, r.Record); err != nil {
+			if err := s.next.Callback(ctx, r.ref.Record); err != nil {
 				return err
 			}
 			continue
 		}
 
-		record := r.Record.NewSlice(r.i, r.i+1)
+		record := r.ref.Record.NewSlice(r.i, r.i+1)
 		defer record.Release()
 		if err := s.next.Callback(ctx, record); err != nil {
 			return err
@@ -201,22 +216,22 @@ func (s *ReservoirSampler) Finish(ctx context.Context) error {
 
 // replace will replace the row at index i with the row in the record r at index j.
 func (s *ReservoirSampler) replace(i int, newRow sample) {
-	s.sizeInBytes -= s.reservoir[i].Release()
+	s.sizeInBytes -= s.reservoir[i].ref.Release()
 	s.reservoir[i] = newRow
-	s.sizeInBytes += newRow.Retain()
+	s.sizeInBytes += newRow.ref.Retain()
 }
 
 // materialize will build a new record from the reservoir to release the underlying records.
 func (s *ReservoirSampler) materialize(allocator memory.Allocator) error {
 	// Build the unified schema for the records
-	schema := s.reservoir[0].Schema()
+	schema := s.reservoir[0].ref.Schema()
 	fields := schema.Fields()
 	added := map[string]struct{}{}
 	for i := 1; i < len(s.reservoir); i++ {
-		for j := 0; j < s.reservoir[i].Schema().NumFields(); j++ {
-			newField := s.reservoir[i].Schema().Field(j).Name
-			if _, ok := added[newField]; !ok && !schema.HasField(s.reservoir[i].Schema().Field(j).Name) {
-				fields = append(fields, s.reservoir[i].Schema().Field(j))
+		for j := 0; j < s.reservoir[i].ref.Schema().NumFields(); j++ {
+			newField := s.reservoir[i].ref.Schema().Field(j).Name
+			if _, ok := added[newField]; !ok && !schema.HasField(s.reservoir[i].ref.Schema().Field(j).Name) {
+				fields = append(fields, s.reservoir[i].ref.Schema().Field(j))
 				added[newField] = struct{}{}
 			}
 		}
@@ -242,12 +257,12 @@ func (s *ReservoirSampler) materialize(allocator memory.Allocator) error {
 	for _, r := range s.reservoir {
 		for i, f := range bldr.Fields() { // TODO handle disparate schemas
 			// Check if this record has this field
-			if !r.Schema().HasField(schema.Field(i).Name) {
+			if !r.ref.Schema().HasField(schema.Field(i).Name) {
 				if err := builder.AppendValue(f, nil, -1); err != nil {
 					return err
 				}
 			} else {
-				if err := builder.AppendValue(f, r.Column(i), int(r.i)); err != nil {
+				if err := builder.AppendValue(f, r.ref.Column(i), int(r.i)); err != nil {
 					return err
 				}
 			}
@@ -256,12 +271,12 @@ func (s *ReservoirSampler) materialize(allocator memory.Allocator) error {
 
 	// Clear the reservoir
 	for _, r := range s.reservoir {
-		s.sizeInBytes -= r.Release()
+		s.sizeInBytes -= r.ref.Release()
 	}
 	// Set the record to be the new reservoir
-	smpl := sample{Record: bldr.NewRecord(), i: -1, ref: refPtr()}
-	s.sizeInBytes += smpl.Retain()
-	smpl.Record.Release() // Release this here because of the retain in the previous line.
+	smpl := sample{i: -1, ref: refPtr(bldr.NewRecord())}
+	s.sizeInBytes += smpl.ref.Retain()
+	smpl.ref.Record.Release() // Release this here because of the retain in the previous line.
 	s.reservoir = []sample{smpl}
 
 	// reslice the reservoir for easy row replacement
